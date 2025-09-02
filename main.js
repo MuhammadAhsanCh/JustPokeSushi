e8c5cb304f0b742beabec59c99c2a5e7a6e389c8
Muhammad Ahsan <ma23awk@herts.ac.uk>
2025-09-02 19:31:23 +0100

    Sliders

---
+++ main.js
@@ -0,0 +1,10 @@
+// Swiper
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSliders: true,
+
+    navigation: {
+        nextE1: ".swiper-button-next",
+        prevE1: ".swiper-button-prev",
+    },
+});
